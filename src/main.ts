fcefa3a6d44093eb9b71b4bc4d3b4ab3ea177a7e
Qusli <quali123@list.ru>
2025-06-10 23:56:42 +0300

    Feat: Обновил конфигурацию приложения, добавил sass как dev зависимость, добавил bootstrap.scss и _reset.scss

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "./assets/styles/bootstrap.scss"
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 433,
+    host: "127.0.0.1",
+    hmr: {
+      host: '127.0.0.1',
+      port: 433,
+    },
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    }
+  }
 })
